724b58b09c42697f02a45e479a278c8e2d4cd8ae
xgyutu <119731546+xgyutu@users.noreply.github.com>
2024-01-31 01:05:20 +0800

    Update main_sceneflow.py

--- main_sceneflow.py
+++ main_sceneflow.py
@@ -1,6 +1,7 @@
 # from __future__ import print_function, division
 import argparse
 import os
+
 os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3'
 import torch
 import torch.nn as nn
@@ -26,22 +27,24 @@ cudnn.benchmark = True
 # import torch.distributed as dist
 # dist.init_process_group('gloo', init_method='file:///tmp/somefile', rank=0, world_size=1)
 
-parser = argparse.ArgumentParser(description='Accurate and Efficient Stereo Matching via Attention Concatenation Volume (Fast-ACV)')
-parser.add_argument('--model', default='srblocknum20', help='select a model structure', choices=__models__.keys())
+parser = argparse.ArgumentParser(description='nextstereo')
+parser.add_argument('--model', default='nextstereo', help='select a model structure', choices=__models__.keys())
 parser.add_argument('--maxdisp', type=int, default=192, help='maximum disparity')
 parser.add_argument('--dataset', default='sceneflow', help='dataset name', choices=__datasets__.keys())
 parser.add_argument('--datapath', default="/root/autodl-tmp/sceneflow/", help='data path')
 parser.add_argument('--trainlist', default='./filenames/sceneflow_train.txt', help='training list')
-parser.add_argument('--testlist',default='./filenames/sceneflow_test.txt', help='testing list')
+parser.add_argument('--testlist', default='./filenames/sceneflow_test.txt', help='testing list')
 parser.add_argument('--lr', type=float, default=0.001, help='base learning rate')
-parser.add_argument('--batch_size', type=int, default=38, help='training batch size')
-parser.add_argument('--test_batch_size', type=int, default=38, help='testing batch size')
+parser.add_argument('--batch_size', type=int, default=8, help='training batch size')
+parser.add_argument('--test_batch_size', type=int, default=8, help='testing batch size')
 parser.add_argument('--epochs', type=int, default=26, help='number of epochs to train')
 # parser.add_argument('--lrepochs', type=str, default="10,14,16,18,20,22,24,26,28,30:2", help='the epochs to decay lr: the downscale rate')
-parser.add_argument('--lrepochs', type=str, default="8,11,14,17,20,23,25,27,29,30:2", help='the epochs to decay lr: the downscale rate')
+parser.add_argument('--lrepochs', type=str, default="8,11,14,17,20,23,25,27,29,30:2",
+                    help='the epochs to decay lr: the downscale rate')
 
-parser.add_argument('--logdir',default='/root/autodl-tmp/runs/sceneflow/NeXt_Stereo/finalpass_finetune/', help='the directory to save logs and checkpoints')
-parser.add_argument('--loadckpt', default='/root/autodl-tmp/runs/sceneflow/NeXt_Stereo/finalpass/checkpoint_000025.ckpt',help='load the weights from a specific checkpoint')
+parser.add_argument('--logdir', default='/root/tf-logs/runs/sceneflow/baseline',
+                    help='the directory to save logs and checkpoints')
+parser.add_argument('--loadckpt', default='', help='load the weights from a specific checkpoint')
 parser.add_argument('--resume', action='store_true', help='continue training the model')
 parser.add_argument('--seed', type=int, default=616, metavar='S', help='random seed (default: 1)')
 parser.add_argument('--summary_freq', type=int, default=100, help='the frequency of saving summary')
@@ -70,7 +73,7 @@ model = __models__[args.model](args.maxdisp)
 model = nn.DataParallel(model)
 model.cuda()
 # optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999))
-optimizer = optim.AdamW(model.parameters(), lr = args.lr, weight_decay=.00001, eps=1e-8)
+optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=.00001, eps=1e-8)
 # load parameters
 start_epoch = 0
 if args.resume:
@@ -90,12 +93,14 @@ elif args.loadckpt:
     state_dict = torch.load(args.loadckpt)
     model_dict = model.state_dict()
     pre_dict = {k: v for k, v in state_dict['model'].items() if k in model_dict}
-    model_dict.update(pre_dict) 
+    model_dict.update(pre_dict)
     model.load_state_dict(model_dict)
+
+
 # print("start at epoch {}".format(start_epoch))
 
 def train():
-    #for epoch_idx in range(start_epoch, args.epochs):
+    # for epoch_idx in range(start_epoch, args.epochs):
     for epoch_idx in range(start_epoch, args.epochs):
         adjust_learning_rate(optimizer, epoch_idx, args.lr, args.lrepochs)
 
@@ -114,19 +119,16 @@ def train():
                                                                                        len(TrainImgLoader), loss,
                                                                                        time.time() - start_time))
 
-            
-
-            
         # saving checkpoints
 
         if epoch_idx % 5 == 0 or (epoch_idx + 1) % args.save_freq == 0:
             checkpoint_data = {'epoch': epoch_idx, 'model': model.state_dict(), 'optimizer': optimizer.state_dict()}
-            #id_epoch = (epoch_idx + 1) % 100
+            # id_epoch = (epoch_idx + 1) % 100
             torch.save(checkpoint_data, "{}/checkpoint_{:0>6}.ckpt".format(args.logdir, epoch_idx))
         gc.collect()
-        
+
         # if epoch_idx == args.epochs - 1:
-        if epoch_idx % 5 == 0 or epoch_idx == args.epochs - 1:    
+        if epoch_idx % 5 == 0 or epoch_idx == args.epochs - 1:
             # # testing
             avg_test_scalars = AverageMeterDict()
             for batch_idx, sample in enumerate(tqdm(TestImgLoader)):
@@ -137,8 +139,8 @@ def train():
                 do_summary = global_step % 1 == 0
                 loss, scalar_outputs, image_outputs = test_sample(sample, compute_metrics=do_summary)
                 if do_summary:
-                   save_scalars(logger, 'test', scalar_outputs, global_step)
-                   save_images(logger, 'test', image_outputs, global_step)
+                    save_scalars(logger, 'test', scalar_outputs, global_step)
+                    save_images(logger, 'test', image_outputs, global_step)
                 avg_test_scalars.update(scalar_outputs)
                 del scalar_outputs, image_outputs
                 print('Epoch {}/{}, Iter {}/{}, test loss = {:.3f}, time = {:3f}'.format(epoch_idx, args.epochs,
@@ -164,7 +166,7 @@ def train_sample(sample, compute_metrics=False):
     mask = (disp_gt < args.maxdisp) & (disp_gt > 0)
     mask_low = (disp_gt_low < args.maxdisp) & (disp_gt_low > 0)
     masks = [mask, mask_low, mask, mask_low]
-    disp_gts = [disp_gt, disp_gt_low, disp_gt, disp_gt_low] 
+    disp_gts = [disp_gt, disp_gt_low, disp_gt, disp_gt_low]
     loss = model_loss_train(disp_ests, disp_gts, masks)
     scalar_outputs = {"loss": loss}
     image_outputs = {"disp_est": disp_ests, "disp_gt": disp_gt, "imgL": imgL, "imgR": imgR}
@@ -181,19 +183,18 @@ def train_sample(sample, compute_metrics=False):
 
     return tensor2float(loss), tensor2float(scalar_outputs), image_outputs
 
+
 # test one sample
 @make_nograd_func
 def test_sample(sample, compute_metrics=True):
     model.eval()
-    model1 = copy.deepcopy(model)
-    model1.eval()
 
     imgL, imgR, disp_gt = sample['left'], sample['right'], sample['disparity']
     imgL = imgL.cuda()
     imgR = imgR.cuda()
     disp_gt = disp_gt.cuda()
     mask = (disp_gt < args.maxdisp) & (disp_gt > 0)
-    disp_ests = model1(imgL, imgR)
+    disp_ests = model(imgL, imgR)
     disp_gts = [disp_gt]
     masks = [mask]
     loss = model_loss_test(disp_ests, disp_gts, masks)
@@ -207,6 +208,7 @@ def test_sample(sample, compute_metrics=True):
     if compute_metrics:
         image_outputs["errormap"] = [disp_error_image_func.apply(disp_est, disp_gt) for disp_est in disp_ests]
     return tensor2float(loss), tensor2float(scalar_outputs), image_outputs
+
 
 if __name__ == '__main__':
     train()
